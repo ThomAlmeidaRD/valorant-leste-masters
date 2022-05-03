afd95336b9367ec1a760e7de6c5dac1826b6f4b4
thomas <thomas.rodrigues@idealtrends.com.br>
2022-05-03 17:40:53 -0300

    estrutura basica e navbar

---
+++ inc/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+    <div class="navbar-content">
+        <ul>
+            <li id="float-left">VLM <b class="italic-purple">2K22</b></li>
+            <li><a href="#">Regulamento</a></li>
+            <li>Valores</li>
+            <li>Quais Elos ?</li>
+            <li>
+                <button class="cta-navbar"> 
+                    Garantir vaga
+                </button>
+            </li>
+        </ul>
+    </div>
+</nav>
