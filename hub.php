d19228d442e3c121a1abb5b164c24ec353a142fe
thomas <thomas.rodrigues@idealtrends.com.br>
2022-05-26 21:02:19 -0300

    Ajustes de CSS

---
+++ hub.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Document</title>
+    <?
+    include('inc/geral.php')
+    ?>
+</head>
+
+<body>
+    <?php
+    include('inc/navbar.php')
+    ?>
+
+
+    <div class="container">
+        <div class="container-content">
+            <?
+            include('inc/hub-banner.php')
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ inc/hub-banner.php
@@ -0,0 +1,42 @@
+<div class="hub-banner-container">
+    <div class="hub-banner-content">
+        <h1>Bem-vindo ao Hub do VLM 2K22!</h1>
+        <p>Aqui vc encontra as informacoes da competicao e do seu time.</p>
+        <br>
+        <h3>Campeonato Termina Em: <b>12 Dias</b></h3>
+    </div>
+</div>
+<div class="match-day-list">
+    <ul>
+        <li id="horario">
+            <div class="match-day-horario">
+                <h3>TimeA</h3>
+                <h3>timeB</h3>
+            </div>
+        </li>
+        <li>
+            <div class="match-day">
+                <h3>TimeA</h3>
+                <h3>timeB</h3>
+            </div>
+        </li>
+        <li>
+            <div class="match-day">
+                <h3>TimeA</h3>
+                <h3>timeB</h3>
+            </div>
+        </li>
+        <li>
+            <div class="match-day">
+                <h3>TimeA</h3>
+                <h3>timeB</h3>
+            </div>
+        </li>
+        <li>
+            <div class="match-day">
+                <h3>TimeA</h3>
+                <h3>timeB</h3>
+            </div>
+        </li>
+    </ul>
+</div>
